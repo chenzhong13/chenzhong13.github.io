84c347fc1006d0c66abca2b2de47ac3e2c23df58
zhongchen <2681636455@qq.com>
2015-10-29 19:13:39 +0800

    fuck commit message!

--- projects/accountkeeper/home/js.js
+++ projects/accountkeeper/home/js.js
@@ -1,9 +1,19 @@
-$(function () {
-    var chart;
-
-    $(document).ready(function () {
-
-        // Build the chart
+var HomeCtrl = {
+    init: function () {
+        /* because loading data will cost too munch time,
+         so we postpone the loading task to avoid blocking
+         UI rendering
+         */
+        var self = this;
+        setTimeout(function () {
+            self.loadData();
+        }, 1000);
+        this.bindEvent();
+    },
+    loadData: function () {
+        this.loadChart();
+    },
+    loadChart: function () {
         $('#monthChart').highcharts({
             chart: {
                 plotBackgroundColor: null,
@@ -17,7 +27,10 @@ $(function () {
                 verticalAlign: 'middle',
                 x: 0,
                 layout: 'vertical',
-                margin: -10000
+                margin: -10000,
+                style: {
+                    color: 'red'
+                }
             },
             title: {
                 text: '',
@@ -59,17 +72,14 @@ $(function () {
 
         var daysOfMonth = [];
         var data1 = [];
-        var data2 = [];
         (function () {
             for (var i = 1; i < 32; i++) {
                 daysOfMonth.push(i);
                 var random = Math.random() * 10 + 20;
-                data1.push({y: parseFloat(random.toFixed(2)), color: (Math.random() < 0.5 ? "#2196f3" : 'red')});
-                random = Math.random() * 10 + 20;
-                data2.push({y: parseFloat(random.toFixed(2)), color: "#00ff00"});
+                data1.push({y: parseFloat(random.toFixed(2)), color: (Math.random() < 0.5 ? "#BBDEFB" : '#FF5252')});
             }
         })();
-        $('#monthDetailChart').highcharts({
+        $('#month-detail-chart').highcharts({
             chart: {
                 type: 'line',
                 backgroundColor: null
@@ -111,68 +121,16 @@ $(function () {
                 data: data1
             }]
         });
-
-        if (1 == 1) {
-            return;
-        }
-        var daysOfMonth = [];
-        var data1 = [];
-        (function () {
-            for (var i = 0; i < 24; i++) {
-                daysOfMonth.push(i);
-                var random = Math.random() * 30 + 0;
-                data1.push({y: parseFloat(random.toFixed(2)), color: "#ff0000"});
-            }
-        })();
-        $('#dayDetailChart').highcharts({
-            chart: {
-                type: 'column',
-                backgroundColor: null
-            },
-            title: {
-                text: ''
-            },
-            xAxis: {
-                categories: daysOfMonth,
-                labels: {
-                    rotation: -45,
-                    align: 'right',
-                    style: {
-                        fontSize: '13px',
-                        fontFamily: 'Verdana, sans-serif'
-                    }
-                }
-            },
-            yAxis: {
-                min: 0,
-                title: {
-                    text: null
-                }
-            },
-            legend: {
-                enabled: false
-            },
-            tooltip: {
-                pointFormat: 'Population in 2008: <b>{point.y:.1f} millions</b>',
-            },
-            series: [{
-                name: 'Population',
-                data: data1,
-                dataLabels: {
-                    enabled: false,
-                    rotation: -90,
-                    color: '#FFFFFF',
-                    align: 'right',
-                    x: 4,
-                    y: 10,
-                    style: {
-                        fontSize: '13px',
-                        fontFamily: 'Verdana, sans-serif',
-                        textShadow: '0 0 3px black'
-                    }
-                }
-            }]
+    },
+    bindEvent: function () {
+        $('.month-card-content').on('click', function () {
+            var url = $(this).attr("data-target-url");
+            window.location.href = url;
         });
-    });
-
-});
+        $('.float-btn').on('click', function () {
+            var url = $(this).attr("data-target-url");
+            window.location.href = url;
+        });
+    }
+};
+$(document).ready(HomeCtrl.init());
